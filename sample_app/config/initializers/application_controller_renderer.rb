096a774b0c1d24e4ccf2d79f73781f5ade632357
HiromuAndo <tqgqxxjuzaa8hbcwjv7n@docomo.ne.jp>
2019-06-09 00:03:12 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1e131af3b4e19e47dacdaa5f71ec3d9441ffc782c1ef02c26610cfa4dc3d448d88f03410ddb72f18126d50c99385954b5da73d6dcc795171cba106adb8d1cb57'
