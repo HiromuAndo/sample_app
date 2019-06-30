4d69a55dd810322e1cf2d990e3194cc20079bee1
HiromuAndo <tqgqxxjuzaa8hbcwjv7n@docomo.ne.jp>
2019-06-30 20:09:26 +0900

    [Update]

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a656661df713ac2abf55705ed9d3e8dfdae207ea234b72470fc7981c34cf56d942d9986ed1346729ca016b81826eced0cca38582af8979c12ddbd4cda133251d'
